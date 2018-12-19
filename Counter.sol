3c46de1b2143f4fedc2db3ccacf509575a713240
Hamzah Chaudhry <hamzah@Hamzah-MBP.attlocal.net>
2018-12-18 19:33:01 -0500

    Added truffle project and Counter smart contract

---
+++ Counter.sol
@@ -0,0 +1,25 @@
+/** https://medium.com/coinmonks/getting-started-with-ethereum-blockchain-development-part-1-d6543b441bea */
+
+pragma solidity ^0.4.23;
+
+contract Counter {
+    
+    event CounterIncrementedEvent(int count);
+    event CounterDecrementedEvent(int count);
+
+    int private count = 0;
+
+    function incrementCounter() public {
+        count += 1;
+        emit CounterIncrementedEvent(count);
+    }
+
+    function decrementCounter() public {
+        count -= 1;
+        emit CounterDecrementedEvent(count);
+    }
+
+    function getCount() public view returns (int) {
+        return count;
+    }
+}
