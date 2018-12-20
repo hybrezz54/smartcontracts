820d37dc19c99a982f29ea2941f827c409a662bd
Hamzah Chaudhry <haz540@gmail.com>
2018-12-20 00:45:35 -0500

    Added wrestler example smart contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 var Counter = artifacts.require("Counter");
-var Greeter = artifacts.require("Greeter")
+var Greeter = artifacts.require("Greeter");
+var Wrestler = artifacts.require("Wrestler");
 
 module.exports = function(deployer) {
   // Use deployer to state migration tasks.
   deployer.deploy(Counter);
   deployer.deploy(Greeter);
+  deployer.deploy(Wrestler);
 };
